a94492fa0f330c6651283c048c2432c8ede5fcfb
imnag.trainer@gmail.com <nagabhushanamn@gmail.com>
2018-07-20 14:40:33 +0530

    after lunch commit

--- sonar-sample-java-appln/src/main/java/com/App.java
+++ sonar-sample-java-appln/src/main/java/com/App.java
@@ -2,23 +2,7 @@ package com;
 
 public class App {
 	
-	
-	// v1
-	public void getFoodV1() {
-		System.out.println("veg");
-		System.out.println("non-veg");
-		System.out.println("veg");
-		System.out.println("non-veg");
-		System.out.println("veg");
-		System.out.println("non-veg");
-		System.out.println("veg");
-		System.out.println("non-veg");
-		System.out.println("veg");
-		System.out.println("non-veg");
-		System.out.println("veg");
-		System.out.println("non-veg");
-	}
-	
+
 	public static int getLen() {
 		String item=null;
 		if(item==null)
@@ -28,13 +12,13 @@ public class App {
 	}
 	
 	public static void main(String[] args) {
+		
 		System.out.println("hello sonar");
 		int v=12;
 		System.out.println(getLen());
 		
-		System.out.println("missing semi-colon");
+		String var="after lunch";
 		
-		String menu="food ready";
 	}
 
 }
